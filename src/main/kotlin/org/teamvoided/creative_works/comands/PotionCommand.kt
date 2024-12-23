67d819b6a70c2a5de5f76cf74e3ac0945a3867d9
TheEnderCore <theendercore@gmail.com>
2024-12-23 17:44:19 +0200

    - Cleaned up pot command

--- src/main/kotlin/org/teamvoided/creative_works/comands/PotionCommand.kt
+++ src/main/kotlin/org/teamvoided/creative_works/comands/PotionCommand.kt
@@ -1,14 +1,14 @@
 package org.teamvoided.creative_works.comands
 
 import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.context.CommandContext
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import net.minecraft.entity.effect.StatusEffect
 import net.minecraft.entity.effect.StatusEffectInstance
-import net.minecraft.entity.effect.StatusEffects
+import net.minecraft.entity.effect.StatusEffects.*
 import net.minecraft.registry.Holder
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
-import org.teamvoided.creative_works.util.childOf
+import org.teamvoided.creative_works.util.buildChildOf
 
 object PotionCommand {
 
@@ -18,30 +18,20 @@ object PotionCommand {
         val root = literal("pot").build()
         dispatcher.root.addChild(root)
 
-        val nvNode = literal("nv")
-            .executes { pot(it, StatusEffects.NIGHT_VISION, 0) }
-            .build()
-            .childOf(root)
-        val stNode = literal("st")
-            .executes { pot(it, StatusEffects.STRENGTH) }
-            .build()
-            .childOf(root)
-        val rsNode = literal("rs")
-            .executes { pot(it, StatusEffects.RESISTANCE) }
-            .build()
-            .childOf(root)
-        val saNode = literal("sa")
-            .executes { pot(it, StatusEffects.SATURATION) }
-            .build()
-            .childOf(root)
+        literal("nv").pot(NIGHT_VISION, 0).buildChildOf(root)
+        literal("st").pot(STRENGTH).buildChildOf(root)
+        literal("rs").pot(RESISTANCE).buildChildOf(root)
+        literal("sa").pot(SATURATION).buildChildOf(root)
 
     }
 
-    private fun pot(c: CommandContext<ServerCommandSource>, effect: Holder<StatusEffect>, amplifier: Int = 255): Int {
-        val player = c.source.player ?: return 0
-        player.addStatusEffect(
-            StatusEffectInstance(effect, -1, amplifier, true, false, false)
-        )
-        return 1
+    private fun LiteralArgumentBuilder<ServerCommandSource>.pot(effect: Holder<StatusEffect>, amplifier: Int = 255)
+            : LiteralArgumentBuilder<ServerCommandSource> {
+        this.executes cmd@{
+            val player = it.source?.player ?: return@cmd 0
+            player.addStatusEffect(StatusEffectInstance(effect, -1, amplifier, true, false, false))
+            return@cmd 1
+        }
+        return this
     }
 }
--- src/main/kotlin/org/teamvoided/creative_works/util/Helper.kt
+++ src/main/kotlin/org/teamvoided/creative_works/util/Helper.kt
@@ -1,11 +1,15 @@
 package org.teamvoided.creative_works.util
 
+import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.tree.CommandNode
 import net.minecraft.text.Text
 
 fun <S> CommandNode<S>.childOf(node: CommandNode<S>): CommandNode<S> {
     node.addChild(this)
     return this
+}
+fun <S> LiteralArgumentBuilder<S>.buildChildOf(node: CommandNode<S>): CommandNode<S> {
+    return this.build().childOf(node)
 }
 
 fun ltxt(s: String)= Text.literal(s)
