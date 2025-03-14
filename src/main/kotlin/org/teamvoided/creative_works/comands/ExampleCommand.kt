c17523bc3c4d67fab1060e3bd7c41a9e54aaf484
TheEnderCore <theendercore@gmail.com>
2025-03-14 18:27:35 +0200

    - extended example command

--- src/main/kotlin/org/teamvoided/creative_works/comands/ExampleCommand.kt
+++ src/main/kotlin/org/teamvoided/creative_works/comands/ExampleCommand.kt
@@ -6,6 +6,7 @@ import com.mojang.brigadier.context.CommandContext
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
 import org.teamvoided.creative_works.util.buildChildOf
+import org.teamvoided.creative_works.util.error
 import org.teamvoided.creative_works.util.message
 
 object ExampleCommand {
@@ -17,7 +18,11 @@ object ExampleCommand {
         val src = ctx.source ?: return 0
         val server = src.server ?: return 0
         val world = src.world ?: return 0
-
+        val target = src.player
+        if (target == null) {
+            src.error("Command has no target!")
+            return 0
+        }
 
         src.message("Exmaple!")
         return Command.SINGLE_SUCCESS
